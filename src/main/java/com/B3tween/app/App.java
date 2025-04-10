0ea254b36d631c43512d5c5d6ec64664f2ec53fa
Daniel López Santiago <A509395@AT4WIRELESS>
2025-04-10 10:51:24 +0200

    Hello :)

---
+++ src/main/java/com/B3tween/app/App.java
@@ -0,0 +1,13 @@
+package com.B3tween.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
