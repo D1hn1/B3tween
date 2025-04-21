417192b2a8f7de22a77b2390c6e57e204e5fd3ec
Daniel López <dani@latop.com>
2025-04-21 09:43:59 +0200

    ✅ ADDED: Listener and handeler

---
+++ src/main/java/com/B3tween/app/modules/handler/listener/Listener.java
@@ -0,0 +1,26 @@
+package com.B3tween.app.modules.handler.listener;
+
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.*;
+
+import com.B3tween.app.modules.log.Log;
+import com.B3tween.app.modules.handler.handleClient.handler;
+
+public class Listener {
+    
+    public Listener(int port) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        Log.i("Server listening on port: " + port);
+
+        ExecutorService pool = Executors.newCachedThreadPool();
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            pool.submit(() -> handler.handleClient(clientSocket));
+        }
+    }
+
+}
