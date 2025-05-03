e281bb827d5d5d7fb84485fcb2a2885bf1e3b9e4
d1hn1 <daniellopezsantiago27@gmail.com>
2025-05-03 16:35:13 +0200

    ✅ ADDED: Fields

--- src/main/java/com/B3tween/app/objects/global/globalRuntime.java
+++ src/main/java/com/B3tween/app/objects/global/globalRuntime.java
@@ -1,5 +1,11 @@
 package com.B3tween.app.objects.global;
 
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Setter;
+import com.B3tween.app.modules.handler.handleConnection.dto.connectionDto;
+
+@Setter
 public class globalRuntime {
 
     // Global username/password
@@ -9,4 +15,9 @@ public class globalRuntime {
     // Booleans
     public static boolean RUNNING = true;
     public static boolean PROXY_AUTHENTICATION = true;
+
+    // connectionDto list
+    public static int connectionId;
+    public static List<connectionDto> connectionList = new ArrayList<>();
+
 }
