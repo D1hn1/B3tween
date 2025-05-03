9d94f01e8f074411c2995dba14deaa13ac49c428
d1hn1 <daniellopezsantiago27@gmail.com>
2025-05-03 16:35:59 +0200

    ✅ UPDATED

--- src/main/java/com/B3tween/app/modules/handler/handleConnection/handler.java
+++ src/main/java/com/B3tween/app/modules/handler/handleConnection/handler.java
@@ -5,8 +5,10 @@ import java.net.*;
 
 import com.B3tween.app.objects.dto.headerDto;
 import com.B3tween.app.objects.dto.requestDto;
+import com.B3tween.app.objects.global.globalRuntime;
 import com.B3tween.app.modules.exception.bException;
 import com.B3tween.app.modules.handler.utils.handlerUtils;
+import com.B3tween.app.modules.handler.handleConnection.dto.connectionDto;
 import com.B3tween.app.modules.log.Log;
 
 public class handler {
@@ -21,7 +23,7 @@ public class handler {
             clientOut = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
         } catch (IOException io) {}
 
-        // Get keep-alive headers
+        // Get Keep-Alive header
         boolean isKeepAlive = false;
         for (headerDto header : request.getHeaders()) {
             if (header.getKey().equals("Proxy-Connection")) {
@@ -31,16 +33,30 @@ public class handler {
             }
         }
 
+        // Connection DTO
+        connectionDto connectionData = connectionDto.builder()
+            .id(handlerUtils.getNextId())
+            .request(request)
+            .clientSocket(clientSocket)
+            .clientIn(clientIn)
+            .clientOut(clientOut)
+            .isKeepAlive(isKeepAlive)
+            .bytesIn(request.getData().length())
+            .build();
+        globalRuntime.connectionList.add(connectionData);
+
         // ADD HTTP/HTTPS SUPPORT
         switch (request.getMethod()) {
             case CONNECT:
                 // ADD FOR JUST NOW THE FORWARD BYTE APROACH
                 // USE THE INITIALIZESOCKET CLASS AND CREATE ANOTHER FOR TLS 
+                httpsProxyHandler.dispatchRequest(connectionData);
                 break;
         
             default:
                 // ADD FOR JUST NOW THE FORWARD DATA
                 // USE THE INITIALIZESOCKET CLASS
+                httpProxyHandler.dispatchRequest(connectionData);
                 break;
         }
 
