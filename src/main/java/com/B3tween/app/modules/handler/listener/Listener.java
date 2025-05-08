bec747e935ae51ae8e772232509e9cab96cd0bf5
d1hn1 <daniellopezsantiago27@gmail.com>
2025-05-08 22:03:07 +0200

    ⬆️  UPDATED: Moved the authentication out of the main thread

--- src/main/java/com/B3tween/app/modules/handler/listener/Listener.java
+++ src/main/java/com/B3tween/app/modules/handler/listener/Listener.java
@@ -5,15 +5,9 @@ import java.net.*;
 import java.util.concurrent.*;
 
 import com.B3tween.app.modules.log.Log;
-import com.B3tween.app.modules.auth.authProxyImpl;
-import com.B3tween.app.objects.dto.headerDto;
-import com.B3tween.app.objects.dto.requestDto;
-import com.B3tween.app.objects.dto.responseDto;
 import com.B3tween.app.objects.global.globalRuntime;
 import com.B3tween.app.modules.handler.handleConnection.handler;
 import com.B3tween.app.modules.handler.utils.handlerUtils;
-import com.B3tween.app.modules.exception.bException;
-import com.B3tween.app.objects.enums.Exceptions;
 
 public class Listener {
     
@@ -49,22 +43,8 @@ public class Listener {
 
             // Accept clients
             Socket clientSocket = serverSocket.accept();
-            requestDto proxyRequest = handlerUtils.getRequest(clientSocket);
+            pool.submit(() -> handler.Handler(clientSocket));
 
-            // Validate proxy authentication
-            if (globalRuntime.PROXY_AUTHENTICATION) {
-
-                if (authProxyImpl.validateLogin(proxyRequest)) {
-                    Log.i("Client: " + clientSocket.getRemoteSocketAddress() + " authenticated");
-                    pool.submit(() -> handler.Handler(clientSocket, proxyRequest));
-                } else {
-                    handlerUtils.responses.proxyAuthenticationRequired(clientSocket);
-                    Log.e("Client: " + clientSocket.getRemoteSocketAddress() + " failed auth");
-                }
-
-            } else {
-                pool.submit(() -> handler.Handler(clientSocket, proxyRequest));
-            }
         }
     }
 
